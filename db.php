68ac1a34914e424412ba61684d4bffbca8bee11b
Jan Düpmeier <j.duepmeier@googlemail.com>
2014-11-19 17:35:09 +0100

    refactor db.php (please check for bugs...)

--- db.php
+++ db.php
@@ -1,214 +1,464 @@
 <?php
 
-$TABLES = array(
-    "peers" => "id",
-    "categories" => "id",
-    "torrents" => "id"
-);
-$VIEWS = array(
-    "torrentcategories" => "id",
-    "newtorrents" => "id"
-);
-
-$http_raw = file_get_contents("php://input");
 
 
-# open db
-$db = new PDO("sqlite:backing.db3");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-        
-$retVal["status"] = "nothing to do";
+function main() {
 
-$tables = array_keys($TABLES);
-foreach ($tables as $table) {
-    $retVal = request($db, $table, $TABLES[$table], $retVal);
-}
+    global $db, $output;
+    global $dbpath;
+    $dbpath = "backing.db3";
 
-$viewKeys = array_keys($VIEWS);
-foreach ($viewKeys as $view) {
-    $viewData = $_GET[$view];
+    $db = new DB();
+    $db->connect();
+    $output = Output::getInstance();
+    $torrent = new Torrent();
+    $category = new Category();
 
-    if (isset($viewData)) {
-	$retVal[$view] = getView($db, $viewData, $view, $TABLES[$view]);
-    }
-}
+    $endpoint = getEndPoint();
+    $value = $_GET[$endpoint];
 
-$cat = $_GET["cat"];
-$torrents = $_GET["torrents"];
-
-
-$catfor = $_GET["catfor"];
-
-if (isset($catfor) && !empty($catfor)) {
-
-    $retVal["categories"] = getView($db, $catfor, "torrentcategories", "id");
-    $retVal["status"] = array(0);
-}
-
-if (isset($torrents) && isset($cat) && !empty($cat)) {
-    if ($cat == "new") {
-	$retVal["torrents"] = getView($db, "*", "newtorrents", "category");
+    if (!empty($value)) {
+	switch ($endpoint) {
+	    case "peers":
+		$torrent->getPeers($value);
+		break;
+	    case "torrents":
+		$torrent->get($value);
+		break;
+	    case "categories":
+		$category->get($value);
+		break;
+	    case "catfor":
+	    case "torrentcategories":
+		$torrent->getCategories($value);
+		break;
+	}
     } else {
-	$retVal["torrents"] = getView($db, $cat, "torrentcategories", "category");
+	switch ($endpoint) {
+	    case "peers":
+		$torrent->getAllPeers();
+		break;
+	    case "torrents":
+		$cat = $_GET["cat"];
+		if (isset($cat) && !empty($cat)) {
+		    $torrent->getByCategory($cat);
+		} else {
+		    $torrent->getAll();
+		}
+		break;
+	    case "categories":
+		$category->getAll();
+		break;
+	    case "torrentcategories":
+		$torrent->getAllCategories();
+		break;
+	    case "newtorrents":
+		$torrent->getNew();
+	}
+    }
+
+
+    $http_raw = file_get_contents("php://input");
+
+    if (isset($http_raw) && !empty($http_raw)) {
+
+	$obj = json_decode($http_raw, true);
+
+	switch ($endpoint) {
+	    case "category-add":
+		$category->add($obj["torrent"], $obj["category"]);
+		break;
+	    case "category-del":
+		$category->delete($obj["torrent"], $obj["category"]);
+		break;
+	    case "torrent-rename":
+		$torrent->rename($obj["id"], $obj["name"]);
+		break;
+	    case "torrent-del":
+		$torrent->delete($obj["id"]);
+		break;
+	}
+    }
+
+    $output->write();
+}
+
+main();
+
+/*
+ * search current end point
+ */
+
+function getEndPoint() {
+    // all end points
+    $api_points = array(
+	"peers",
+	"categories",
+	"torrents",
+	"torrentcategories",
+	"newtorrents",
+	"category-add",
+	"torrent-del",
+	"category-del",
+	"torrent-rename",
+	"torrent-add"
+    );
+
+    foreach ($api_points as $point) {
+	if (isset($_GET[$point])) {
+	    return $point;
+	}
     }
 }
 
+class Category {
 
+    function getAll() {
+	global $output, $db;
 
+	$query = "SELECT * FROM categories";
+	$params = array();
 
-if (isset($http_raw) && !empty($http_raw)) {
+	$stmt = $db->query($query, $params);
 
-    $obj = json_decode($http_raw, true);
+	$output->addStatus("categories", $stmt->errorInfo());
+	$output->add("categories", $stmt->fetchAll(PDO::FETCH_ASSOC));
 
-    if (isset($_GET["category-add"])) {
-
-	$retVal["status"] = addCatMapping($db, $obj["torrent"], $obj["category"]);
-    }
-    if (isset($_GET["category-del"])) {
-	$retVal["status"] = delCatMapping($db, $obj["torrent"], $obj["category"]);
-    }
-    if (isset($_GET["torrent-del"])) {
-	$retVal["status"] = delTorrent($db, $obj["id"]);
+	$stmt->closeCursor();
     }
 
-    if (isset($_GET["torrent-rename"])) {
-	$retVal["status"] = renameTorrent($db, $obj["id"], $obj["name"]);
-    }
-}
+    function get($id) {
+	global $output, $db;
 
+	$query = "SELECT * FROM categories WHERE id = :id";
+	$params = array(
+	    ":id" => $id
+	);
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-# Rückmeldung senden
+	$stmt = $db->query($query, $params);
 
-if (isset($_GET["callback"]) && !empty($_GET["callback"])) {
-    $callback = $_GET["callback"];
-    echo $callback . "('" . json_encode($retVal, JSON_NUMERIC_CHECK) . "')";
-} else {
-    echo json_encode($retVal, JSON_NUMERIC_CHECK);
-}
+	$output->addStatus("categories", $stmt->errorInfo());
+	$output->add("categories", $stmt->fetchAll(PDO::FETCH_ASSOC));
 
-
-function renameTorrent($db, $id, $name) {
-
-    $query = "UPDATE torrents SET name = :name WHERE id= :id";
-
-    $stm = $db->prepare($query);
-    $stm->execute(array(
-	":id" => $id,
-	":name" => $name
-    ));
-
-    $retVal = $stm->errorInfo();
-
-    $stm->closeCursor();
-    return $retVal;
-}
-
-function delTorrent($db, $torrent) {
-    $query = "DELETE FROM torrents WHERE id = :torrent";
-
-    $stm = $db->prepare($query);
-    $stm->execute(array(
-	":torrent" => $torrent
-    ));
-
-    $retVal = $stm->errorInfo();
-
-    $stm->closeCursor();
-    return $retVal;
-}
-
-function addCatMapping($db, $torrent, $cat) {
-
-    $query = "INSERT INTO categorymap (torrent, category) VALUES(:torrent, :cat)";
-
-    $stm = $db->prepare($query);
-    $stm->execute(array(
-	":torrent" => $torrent,
-	":cat" => $cat
-    ));
-
-    $retVal = $stm->errorInfo();
-
-    $stm->closeCursor();
-    return $retVal;
-}
-
-function delCatMapping($db, $torrent, $cat) {
-
-    $query = "DELETE FROM categorymap WHERE torrent = :torrent AND category = :cat";
-
-    $stm = $db->prepare($query);
-    $stm->execute(array(
-	":torrent" => $torrent,
-	":cat" => $cat
-    ));
-
-    $retVal = $stm->errorInfo();
-
-    $stm->closeCursor();
-    return $retVal;
-}
-
-function getView($db, $data, $tag, $search) {
-
-    if (empty($data) || $data == "*") {
-	$stm = $db->prepare("SELECT * FROM [" . $tag . "]");
-	$stm->execute();
-    } else {
-	$stm = $db->prepare("SELECT * FROM [" . $tag . "] WHERE " . $search . " = :data");
-	$stm->execute(array(
-	    ':data' => $data
-	));
-    }
-    $retVal = $stm->fetchAll(PDO::FETCH_ASSOC);
-    $stm->closeCursor();
-
-    return $retVal;
-}
-
-function getTables($db) {
-
-    $tablesquery = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
-    $i = 0;
-    $tablesRaw = $tablesquery->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($tablesRaw as $table) {
-
-	$tables[$i] = $table['name'];
-	$i++;
+	$stmt->closeCursor();
     }
 
-    $tablesquery->closeCursor();
+    function add($torrent, $category) {
+	global $db, $output;
 
-    return $tables;
+	$query = "INSERT INTO categorymap (torrent, category) VALUES(:torrent, :cat)";
+
+	$params = array(
+	    ":torrent" => $torrent,
+	    ":cat" => $category
+	);
+
+	$stmt = $db->execute($query, $params);
+
+	$output->addStatus("add-category", $stmt->errorInfo());
+
+	$stmt->closeCursor();
+    }
+
+    function delete($torrent, $category) {
+	global $db, $output;
+	$query = "DELETE FROM categorymap WHERE torrent = :torrent AND category = :cat";
+
+	$params = array(
+	    ":torrent" => $torrent,
+	    ":cat" => $category
+	);
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("del-category", $stmt->errorInfo());
+
+	$stmt->closeCursor();
+    }
+
 }
 
-function request($db, $tag, $searchTag, $retVal) {
+class Torrent {
 
-    $tagObject = $_GET[$tag];
+    function delete($torrent) {
+	global $db, $output;
 
-    if (isset($tagObject)) {
+	$query = "DELETE FROM torrents WHERE id = :torrent";
 
-	// options
-	if (!empty($tagObject)) {
-	    $STMT = $db->prepare("SELECT * FROM " . $tag . " WHERE " . $searchTag . " = ?");
-	    $STMT->execute(array($tagObject));
+	$param = array(
+	    ":torrent" => $torrent
+	);
+
+	$stmt = $db->query($query, $param);
+
+	$output->addStatus("del-torrent", $stm->errorInfo());
+
+	$stmt->closeCursor();
+    }
+
+    function rename($id, $name) {
+
+	global $db, $output;
+
+	$query = "UPDATE torrents SET name = :name WHERE id= :id";
+
+	$param = array(
+	    ":id" => $id,
+	    ":name" => $name
+	);
+
+	$stmt = $db->query($query, $param);
+
+	$output->addStatus("rename-torrent", $stmt->errorInfo());
+
+	$stmt->closeCursor();
+    }
+
+    function getAll() {
+	global $output, $db;
+
+	$query = "SELECT * FROM torrents";
+	$params = array();
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("torrents", $stmt->errorInfo());
+	$output->add("torrents", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
+    }
+
+    function get($id) {
+	global $output, $db;
+
+	$query = "SELECT * FROM torrents WHERE id = :id";
+	$params = array(
+	    ":id" => $id
+	);
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("torrents", $stmt->errorInfo());
+	$output->add("torrents", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
+    }
+
+    function getNew() {
+	global $output, $db;
+
+	$query = "SELECT * FROM newtorrents";
+	$params = array();
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("torrents", $stmt->errorInfo());
+	$output->add("torrents", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
+    }
+
+    function getPeers($id) {
+	global $output, $db;
+
+	$query = "SELECT * FROM peers WHERE id = :id";
+	$params = array(
+	    ":id" => $id
+	);
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("peers", $stmt->errorInfo());
+	$output->add("peers", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
+    }
+
+    function getAllPeers() {
+	global $output, $db;
+
+	$query = "SELECT * FROM peers";
+	$params = array();
+
+	$stmt = $db->query($query, $params);
+
+	$output->addStatus("peers", $stmt->errorInfo());
+	$output->add("peers", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
+    }
+
+    function getByCategory($category) {
+	global $output, $db;
+
+	if ($category === "new") {
+	    $query = "SELECT * FROM newtorrents";
+	    $params = array();
 	} else {
-	    $STMT = $db->query("SELECT * FROM " . $tag);
+
+
+	    $query = "SELECT * FROM torrentcategories WHERE category = :category";
+	    $params = array(
+		":category" => $category
+	    );
 	}
 
-	if ($STMT !== FALSE) {
+	$stmt = $db->query($query, $params);
 
-	    $retVal[$tag] = $STMT->fetchAll(PDO::FETCH_ASSOC);
-	} else {
-	    $retVal["status"] = "Failed to create statement";
-	}
-	$STMT->closeCursor();
+	$output->addStatus("torrents", $stmt->errorInfo());
+	$output->add("torrents", $stmt->fetchAll(PDO::FETCH_ASSOC));
+
+	$stmt->closeCursor();
     }
-    
-return $retVal;
+
 }
 
+class DB {
+
+    private $db;
+    private $order = "";
+
+    function connect() {
+	global $dbpath;
+	
+	try {
+	    $this->db = new PDO('sqlite:' . $dbpath);
+	    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	} catch (PDOException $e) {
+
+	    header("Status: 500 " . $e->getMessage());
+	    echo $e->getMessage();
+	    die();
+	}
+
+	return true;
+    }
+
+    function setOrder($tag, $order) {
+	$this->order = " ORDER BY " . $tag . " " . $order;
+    }
+
+    function query($sql, $params) {
+	global $output, $orderBy;
+
+	if (strpos($sql, "SELECT") !== false) {
+
+	    $sql .= $this->order;
+
+	    if (isset($_GET["limit"]) && !empty($_GET["limit"])) {
+		$sql .= " LIMIT :limit";
+		$params[":limit"] = $_GET["limit"];
+	    }
+	}
+
+	$stm = $this->db->prepare($sql);
+
+	if ($this->db->errorCode() > 0) {
+	    $output->addStatus("db", $this->db->errorInfo());
+	    return null;
+	}
+
+	$stm->execute($params);
+
+
+	return $stm;
+    }
+
+    function beginTransaction() {
+	global $output;
+	if (!$this->db->beginTransaction()) {
+	    $output->addStatus("transaction", $this->db->errorInfo());
+	}
+    }
+
+    function commit() {
+	global $output;
+	if (!$this->db->commit()) {
+	    $output->addStatus("commit", $this->db->errorInfo());
+	}
+    }
+
+    function rollback() {
+	$this->db->rollback();
+    }
+
+    function lastInsertID() {
+	return $this->db->lastInsertId();
+    }
+
+}
+
+/**
+ * output functions
+ */
+class Output {
+
+    private static $instance;
+    public $retVal;
+
+    /**
+     * constructor
+     */
+    private function __construct() {
+	$this->retVal['status']["db"] = "ok";
+    }
+
+    /**
+     * Returns the output instance or creates it.
+     * @return Output output instance
+     */
+    public static function getInstance() {
+	if (!self::$instance) {
+	    self::$instance = new self();
+	}
+
+	return self::$instance;
+    }
+
+    /**
+     * Adds data for use to output.
+     * @param type $table
+     * @param type $output
+     */
+    public function add($table, $output) {
+	$this->retVal[$table] = $output;
+    }
+
+    /**
+     * Adds an status for output
+     * @param type $table status table
+     * @param type $output message (use an array with 3 entries ("id", <code>, <message>))
+     */
+    public function addStatus($table, $output) {
+
+	if (is_array($output) && $output[1]) {
+	    if (is_array($retVal["status"]["debug"])) {
+		$this->retVal["status"]["debug"][] = $output;
+	    } else {
+		$retVal["status"]["debug"] = array($output);
+	    }
+	    $this->retVal["status"]["db"] = "failed";
+	}
+
+	$this->retVal['status'][$table] = $output;
+    }
+
+    /**
+     * Generates the output for the browser. General you call this only once.
+     */
+    public function write() {
+
+	header("Content-Type: application/json");
+	header("Access-Control-Allow-Origin: *");
+	# Rückmeldung senden
+	if (isset($_GET["callback"]) && !empty($_GET["callback"])) {
+	    $callback = $_GET["callback"];
+	    echo $callback . "('" . json_encode($this->retVal, JSON_NUMERIC_CHECK) . "')";
+	} else {
+	    echo json_encode($this->retVal, JSON_NUMERIC_CHECK);
+	}
+    }
+
+}
 
 ?>
